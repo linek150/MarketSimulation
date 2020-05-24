66b8b1d4da3d420a4514b5ba82b519a970a25016
linek150 <linek11@vp.pl>
2020-05-24 07:43:13 +0200

    Static files and index displays corectly

--- marketSessions/views.py
+++ marketSessions/views.py
@@ -1,3 +1,20 @@
+import datetime
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+def index(request):
+    if request.user.is_authenticated:
+        pass
+        #if request.
+        #HttpResponseRedirect(reverse('all-borrowed'))
+    #Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    context = {
+        'num_visits': num_visits,
+    }
+    return render(request,'index.html',context=context)
